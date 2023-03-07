b5af1b2209b838ed109f1dbfdbe43d2a081be5dd
Nick <n.kukarkin1991@gmail.com>
2023-03-07 19:20:58 +0300

    первый коммит

---
+++ script.js
@@ -0,0 +1,15 @@
+let  second = 2;
+const pi = 3.14;
+console.log(second, ",", pi);
+
+let answer = alert("Привет!")
+let secondAnsver = confirm("Тебе есть 18?")
+let thirdAnswer = prompt("В каком городе ты живешь?", "Казань")
+
+console.log(answer)
+console.log(secondAnsver)
+console.log(thirdAnswer)
+console.log(thirdAnswer)
+
+
+
