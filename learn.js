196aa3493a2605c15525ede152862882edfae827
Nick <n.kukarkin1991@gmail.com>
2023-03-11 19:10:04 +0300

    простое задание к уроку номер 2

--- learn.js
+++ learn.js
@@ -1,5 +1,16 @@
-let a = 5;
-const b = "Строка";
-let c = [];
+let title = "Название проекта";
+let screens = "Простые, Сложные, Интерактивные".toLowerCase().split();
+let screenPrice = 100;
+let rollback = 15;
+let fullPrice = 1000000;
+let adaptive = true;
 
-console.log("Hello world!")
+console.log(typeof(title));
+console.log(typeof(fullPrice));
+console.log(typeof(adaptive));
+console.log(screens.length);
+console.log("Стоимость верстки экранов " +  screenPrice +  " рублей");
+console.log("Стоимость разработки сайтов " +  fullPrice +  " рублей");
+console.log(screens);
+console.log(fullPrice*(rollback/100))
+
