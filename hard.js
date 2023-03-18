cbabe9ee4557f53d676943c7bc0b4b535751464b
Nick <n.kukarkin1991@gmail.com>
2023-03-18 20:39:16 +0300

    Сделал ветвление с if и switch- поправил

--- hard.js
+++ hard.js
@@ -9,7 +9,25 @@
 
 let days = [["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"]];
 let lang = "ru";
-let result1 = lang === "ru" ? console.log(days[1][0]) : 
+
+let result1 = lang === "ru" ? console.log(days[1]) : 
 lang === "en" ? console.log(days[0]) : 
 console.log("Невалидное значение");
 
+if (lang = "en") {
+    console.log(days[0])
+} else if ( lang = "ru") {
+    console.log(days[1])
+};
+
+switch (lang) {
+    case "ru":
+        console.log(days[1])
+    break;
+    case "en":
+        console.log(days[0])
+    break;
+    default:
+        console.log("Невалидное значение");
+
+}
