ea75d928ad656241e8dd0a3d5fcf7696fa7e3977
Nick <n.kukarkin1991@gmail.com>
2023-03-18 20:30:18 +0300

    Сделал ветвление с if и switch

--- hard.js
+++ hard.js
@@ -1,13 +1,15 @@
-let num = 266219;
+// let num = 266219;
 
-let result = ((num + "").split("")).reduce((sum, current) => sum * current, 1);
+// let result = ((num + "").split("")).reduce((sum, current) => sum * current, 1);
 
-let i = result ** 3;
+// let i = result ** 3;
 
-console.log((i + "").substring(0, 2))
-console.log(result)
+// console.log((i + "").substring(0, 2))
+// console.log(result)
 
 let days = [["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"]];
-let lang = ru;
-let resultWeek = lang = ru ? days[1] : lang = en ? days[0] : "Невалидное значение";
-console.log(resultWeek);
+let lang = "ru";
+let result1 = lang === "ru" ? console.log(days[1][0]) : 
+lang === "en" ? console.log(days[0]) : 
+console.log("Невалидное значение");
+
